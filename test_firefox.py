0b1f9c1afcd45a4b80b34de7c4fbeb62855bc4e7
zenadazurli <zenadazurli@libero.it>
2026-05-23 18:52:37 +0200

    Update test_firefox.py

--- test_firefox.py
+++ test_firefox.py
@@ -2,7 +2,7 @@ from seleniumbase import SB
 
 PROXY = "http://sazz16014w96:t3vz152mql23@resi.fusionproxy.net:13822"
 
-with SB(uc=True, headless=True, xvfb=True, browser="firefox", proxy=PROXY) as sb:
+with SB(uc=True, headless=True, xvfb=True, proxy=PROXY) as sb:
     sb.open("https://api.ipify.org")
     ip = sb.get_text("body")
-    print(f"✅ IP: {ip}")
+    print(f"✅ IP del browser: {ip}")
